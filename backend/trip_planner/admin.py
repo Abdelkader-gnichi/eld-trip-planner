19583922ccd777677c6e691c3d4cff83ae315330
Abdelkader-gnichi <ggnichi097@gmail.com>
2025-03-17 12:17:36 +0100

    register our Trip RoutePoint and ELDLog models with the admin site

--- backend/trip_planner/admin.py
+++ backend/trip_planner/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Trip, RoutePoint, ELDLog
 
-# Register your models here.
+@admin.register(Trip)
+class TripAdmin(admin.ModelAdmin):
+    list_display = ('id', 'current_location', 'pickup_location', 'dropoff_location', 'created_at')
+    search_fields = ('current_location', 'pickup_location', 'dropoff_location')
+
+@admin.register(RoutePoint)
+class RoutePointAdmin(admin.ModelAdmin):
+    list_display = ('id', 'trip', 'point_type', 'location', 'arrival_time', 'departure_time')
+    list_filter = ('point_type',)
+    search_fields = ('location',)
+
+@admin.register(ELDLog)
+class ELDLogAdmin(admin.ModelAdmin):
+    list_display = ('id', 'trip', 'log_date', 'starting_location', 'ending_location')
+    list_filter = ('log_date',)
+    search_fields = ('starting_location', 'ending_location')
